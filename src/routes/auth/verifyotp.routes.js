f33b0e27c6fb9dc62562e9277055651ac461f00c
Prabej Hussain <contact.prabej@gmail.com>
2024-08-01 09:30:30 +0545

    User verification update

--- src/routes/auth/verifyotp.routes.js
+++ src/routes/auth/verifyotp.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const { getData } = require("../../services/auth");
-
+const User = require("../../models/user.model");
 const verifyotp = Router();
 
 verifyotp.post("/", (req, res) => {
   (async () => {
-    const { userOtp, otpToken } = req.body;
+    const { userOtp, otpToken, token } = req.body;
+    const userid = getData(token)._id;
     const otp = getData(otpToken);
     if (otp !== 1) {
       if (otp == userOtp) {
+        await User.updateOne({ _id: userid }, { $set: { isVerified: true } });
         return res.status(200).json({ message: "Successfully verified!" });
       } else {
         return res.status(404).json({ error: "Invlid OTP" });
